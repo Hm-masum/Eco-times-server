37d51961e3b3eda9310ee4d6226e7fac77b2d7a6
Hm-masum <hhmasum88@gmail.com>
2024-06-12 11:17:54 +0600

    mogodb setup commit

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxuuz57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const newsCollection=client.db('ecoTimes').collection('news')
+  
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
