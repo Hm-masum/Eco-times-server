3786ae38617259d13d33e1373bf704d96cdcc791
Hm-masum <hhmasum88@gmail.com>
2024-06-20 00:39:21 +0600

    get article and get article by id commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -48,9 +48,22 @@ async function run() {
     }
 
 
+    // Article related db
     app.post('/article',async(req,res)=>{
       const articleData=req.body;
       const result = await newsCollection.insertOne(articleData)
+      res.send(result)
+    })
+
+    app.get('/article',async(req,res)=>{
+      const result=await newsCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/article/:id', async(req,res)=>{
+      const id= req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result=await newsCollection.findOne(query)
       res.send(result)
     })
   
