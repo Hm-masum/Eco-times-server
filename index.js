93073562333eb0c0afdf8c4ea8625e1e651b8669
Hm-masum <hhmasum88@gmail.com>
2024-06-21 20:07:25 +0600

    users patch commit

--- index.js
+++ index.js
@@ -80,6 +80,23 @@ async function run() {
       const result=await usersCollection.insertOne(user)
       res.send(result)
     })
+
+    app.get('/users',async(req,res)=>{
+      const result=await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id= req.params.id;
+      const filter={_id: new ObjectId(id)}
+      const updatedDoc={
+       $set:{
+          role: 'admin'
+       }
+      }
+      const result = await usersCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+   })
   
 
 
